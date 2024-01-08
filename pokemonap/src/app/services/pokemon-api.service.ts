c6f25e403ca14f11f1f145316fdd8894b5bd0cd7
Alessandro <101320750+Alex-0x@users.noreply.github.com>
2024-01-08 09:48:30 +0100

    add dataStorage-cache

--- pokemonap/src/app/services/pokemon-api.service.ts
+++ pokemonap/src/app/services/pokemon-api.service.ts
@@ -7,20 +7,37 @@ import {
   IPokemonData,
   IResult,
 } from '../interface/pokemon.interface';
-import { Observable } from 'rxjs';
+import { Observable, from, merge } from 'rxjs';
 import { Pokemon } from '../models/Pokemon';
+import { Storage } from '@ionic/storage-angular';
+
+const POKEMON_KEY = 'pokemons';
 
 @Injectable({
   providedIn: 'root',
 })
 export class PokemonApiService {
-  constructor(public http: HttpClient) {}
+  constructor(private http: HttpClient, private storage: Storage) {
+    this.storage.create();
+  }
 
   getPokemons(): Observable<Pokemon[]> {
     const url = environment.pokeUrl + '?limit=' + environment.limit;
-    return this.http.get<IResult>(url).pipe(
+    const cacheData = from(this.storage.get(POKEMON_KEY));
+    cacheData.subscribe((res) => {
+      console.log('cache', res);
+    });
+    return merge(cacheData, this.http.get<IResult>(url)).pipe(
       //sono due map diversi il primo di rjsx e l'altro di js
-      map((res: IResult) => res.results.map((res) => new Pokemon(res))),
+      // il merge unisce i due dati degli observable
+      map((res: IResult) => {
+        if (!res) {
+          return [];
+        }
+        // Se abbiamo una risposta, mettiamo i dati nella cache
+        this.storage.set(POKEMON_KEY, res);
+        return res.results.map((pokemonData) => new Pokemon(pokemonData));
+      }),
       tap((res: Pokemon[]) => console.log(res))
     );
   }
